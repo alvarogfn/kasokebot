6dea96698138168b71003033abcdf1ffc7d2ad1e
Kasoke <62889807+alvarogfn@users.noreply.github.com>
2020-07-12 13:15:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tweepy
 from os import environ
-from boto.s3.connection import S3Connection
 from time import sleep
 from datetime import datetime
 import json
@@ -61,10 +60,10 @@ class stalker(tweepy.StreamListener):
         if status_error == 420:
             return False
     
-CONSUMER_KEY = S3Connection(environ['CONSUMER_KEY'])
-CONSUMER_SECRET = S3Connection(environ['CONSUMER_SECRET'])
-ACCESS_KEY = S3Connection(environ['ACESS_KEY'])
-ACCESS_SECRET = S3Connection(environ['ACESS_SECRET'])
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACESS_KEY']
+ACCESS_SECRET = environ['ACESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
