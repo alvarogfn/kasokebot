ea6ae9c8dae1be5a4c80ce51110453d64064022c
Kasoke <62889807+alvarogfn@users.noreply.github.com>
2020-07-15 19:10:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import functions
 from random import randint, choice
 from datetime import datetime
 
-class narubot(tweepy.StreamListener):
+class kasokebot(tweepy.StreamListener):
     def __init__(self, CONSUMER_KEY, CONSUMER_SECRET, ACESS_KEY, ACESS_SECRET):
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACESS_KEY, ACESS_SECRET)
@@ -20,77 +20,62 @@ class narubot(tweepy.StreamListener):
         rt_test = functions.no_text(status.text)
         
         if rt_test('RT'):   
-            text_tweet = status.text
             id_tweet = status.id
             user = status.user.id
             screen_name = status.user.screen_name
-            
-            self.informations = {"tweet": text_tweet, "tweet_id": id_tweet, "user_id": user, "user_name": screen_name}
-            
+            self.informations = {"tweet_id": id_tweet, "user_id": user, "user_name": screen_name}
             return False
 
     def on_error(self, status_code):
         if status_code == 420:
             self.status = True
             return False
-
-
-    def following(self, user_name):
-        pass
     
     def retweet(self, tweet_id):
-        
         try:
             self.api.retweet(tweet_id)
-            
         except tweepy.RateLimitError:
-            print(f'    Limite de retweets exedido.')
-            
-        except Exception as error:
-            print(f'    Não foi possível retweetar.\nERROR = [{error}]')
-        
+            print(f'    Retweet limit exceeded!')   
+        except:
+            print(f'    Retweet failed!')
         else:         
-            print(f'    Re-tweet!')
+            print(f'    Retweet done!')
     
     def favorite(self, tweet_id):
-        
         try: 
             self.api.create_favorite(tweet_id)
         
         except tweepy.RateLimitError:
-            print(f'    Limite de favoritos exedido.')
-            
-        except Exception as error:
-            print(f'    Não foi possível favoritar.\nERROR = [{error}]')
-        
+            print(f'    Favorite limit exceeded!')    
+        except:
+            print(f'    Favorite failed!')   
         else:         
-            print(f'    Tweet favoritado!')
-    
-    def replying(self, tweet_id, user_name):
-        pass
-    
-    def update_status(self, text):
-        pass
+            print(f'    Favorite done!')
 
 
-print('Start NaruBot!')
+print('Start main script!')
 
 CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']        # Get environment variables
 ACESS_KEY = environ['ACESS_KEY']
 ACESS_SECRET = environ['ACESS_SECRET']
 
-narubot = narubot(CONSUMER_KEY, CONSUMER_SECRET, ACESS_KEY, ACESS_SECRET)
+narubot = kasokebot(CONSUMER_KEY, CONSUMER_SECRET, ACESS_KEY, ACESS_SECRET)
 
-loop = 1
-fav_limit = 100
-ret_limit = 100
+fav_limit = 100                     
+ret_limit = 100                                 # Declaring execution limit variables
 
 tracker = ['Dattebayo', 'Dattebayo!']
 
+loop = 1    #Number of processes done
+
 while True:
+    narubot.status == False                 # Declaring error status as false
+    
     while True:
+        
         narubot.streaming(tracker)
+        
         if narubot.status:
             break
 
@@ -98,19 +83,19 @@ while True:
         sleep(1)
         
         if ret_limit > 0:
-            narubot.retweet(narubot.informations['tweet_id'])
+            narubot.retweet(narubot.informations['tweet_id'])           # Retweeting
             sleep(1)
             ret_limit -= 1
 
         if fav_limit > 0:
-            narubot.favorite(narubot.informations['tweet_id'])
+            narubot.favorite(narubot.informations['tweet_id'])          # Favoring
             sleep(1)
             fav_limit -= 1
         
-        if datetime.now().hour == 0:
+        if datetime.now().hour == 0:                        # Resetting the operations limit
             ret_limit = 100
             fav_limit = 100
         
         sleep(1)
-    print('NaruBot desligado por 15 minutos!')
+    print('>>>>>>>>>>>> Paused for 15 minutes! <<<<<<<<<<<<<<')             # Pause
     sleep(900)
